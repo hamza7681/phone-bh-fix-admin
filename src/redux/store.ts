10d091cd4e12d9909c4847d17fd6293c7597315b
Hamza Siddique <hamzambf@gmail.com>
2024-02-15 13:29:29 +0000

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,28 +1,25 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import AuthSlice from './reducers/authslice'
-import storage from './customStorage'
-import { persistReducer } from 'redux-persist'
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import AuthSlice from "./reducers/authslice";
+import storage from "./customStorage";
+import { persistReducer } from "redux-persist";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const authPersistConfig = {
-  key: 'auth',
+  key: "auth",
   storage: storage,
-  whitelist: ['token', 'user'],
-}
-
+  whitelist: ["token", "user"],
+};
 const rootReducer = combineReducers({
   auth: persistReducer(authPersistConfig, AuthSlice),
-})
-
+});
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-})
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
