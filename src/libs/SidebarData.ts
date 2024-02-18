4364b366c0ca8092eac396909ac295ca70546afa
hamza7681 <hamzambf@gmail.com>
2024-02-18 10:20:14 +0500

    Brands removed

--- src/libs/SidebarData.ts
+++ src/libs/SidebarData.ts
@@ -1,92 +1,92 @@
-import { AiOutlineDashboard } from 'react-icons/ai'
-import { FiUser, FiUserPlus, FiUsers } from 'react-icons/fi'
-import { TbCategory, TbTablePlus } from 'react-icons/tb'
-import { MdFormatListBulleted, MdOutlineCategory } from 'react-icons/md'
-import { GoChecklist } from 'react-icons/go'
-import { MdFormatListBulletedAdd } from 'react-icons/md'
-import { BsCart4, BsCartPlus } from 'react-icons/bs'
-import { HiOutlineShoppingBag } from 'react-icons/hi2'
+import { AiOutlineDashboard } from "react-icons/ai";
+import { FiUser, FiUserPlus, FiUsers } from "react-icons/fi";
+import { TbCategory, TbTablePlus } from "react-icons/tb";
+import { MdFormatListBulleted, MdOutlineCategory } from "react-icons/md";
+import { GoChecklist } from "react-icons/go";
+import { MdFormatListBulletedAdd } from "react-icons/md";
+import { BsCart4, BsCartPlus } from "react-icons/bs";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 export const SidebarData: any[] = [
   {
-    name: 'Dashboard',
-    path: '/dashboard',
+    name: "Dashboard",
+    path: "/dashboard",
     icon: AiOutlineDashboard,
     children: [],
   },
   {
-    name: 'Users',
+    name: "Users",
     icon: FiUsers,
     isChildOpen: false,
     children: [
       {
-        name: 'User List',
-        path: '/users/user-list',
+        name: "User List",
+        path: "/users/user-list",
         icon: FiUser,
       },
       {
-        name: 'Add User',
-        path: '/users/user-add',
+        name: "Add User",
+        path: "/users/user-add",
         icon: FiUserPlus,
       },
     ],
   },
   {
-    name: 'Products',
+    name: "Products",
     icon: HiOutlineShoppingBag,
     isChildOpen: false,
     children: [
       {
-        name: 'Products List',
-        path: '/products/product-list',
+        name: "Products List",
+        path: "/products/product-list",
         icon: BsCart4,
       },
       {
-        name: 'Add Product',
-        path: '/products/product-add',
+        name: "Add Product",
+        path: "/products/product-add",
         icon: BsCartPlus,
       },
     ],
   },
   {
-    name: 'Categories',
+    name: "Categories",
     icon: MdOutlineCategory,
     isChildOpen: false,
     children: [
       {
-        name: 'Categories List',
-        path: '/categories/category-list',
+        name: "Categories List",
+        path: "/categories/category-list",
         icon: TbCategory,
       },
       {
-        name: 'Add Category',
-        path: '/categories/category-add',
+        name: "Add Category",
+        path: "/categories/category-add",
         icon: TbTablePlus,
       },
     ],
   },
+  // {
+  //   name: 'Brands',
+  //   icon: GoChecklist,
+  //   isChildOpen: false,
+  //   children: [
+  //     {
+  //       name: 'Brands List',
+  //       path: '/brands/brand-list',
+  //       icon: MdFormatListBulleted,
+  //     },
+  //     {
+  //       name: 'Add Brand',
+  //       path: '/brands/brand-add',
+  //       icon: MdFormatListBulletedAdd,
+  //     },
+  //   ],
+  // },
   {
-    name: 'Brands',
+    name: "Contact Support",
     icon: GoChecklist,
-    isChildOpen: false,
-    children: [
-      {
-        name: 'Brands List',
-        path: '/brands/brand-list',
-        icon: MdFormatListBulleted,
-      },
-      {
-        name: 'Add Brand',
-        path: '/brands/brand-add',
-        icon: MdFormatListBulletedAdd,
-      },
-    ],
-  },
-  {
-    name: 'Contact Support',
-    icon: GoChecklist,
-    path: '/contact-support',
+    path: "/contact-support",
     isChildOpen: false,
     children: [],
   },
-]
+];
